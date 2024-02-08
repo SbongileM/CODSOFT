55d9405f11d0c7a0c17d5cb112c72cdf9a2c4e69
SbongileM <Sbongilep4@gmail.com>
2024-02-08 20:44:26 +0200

    Refactoring

--- Password_generator/PasswordGenerator.py
+++ Password_generator/PasswordGenerator.py
@@ -8,35 +8,35 @@ to specify the desired length of the password and the complexity.Uses a combinat
 characters to generate a password of the specified length. Then prints the generated password 
 on the screen.The user has the freedom to select the type of characters they want to include
 in the password.
-
 '''
-from PyQt5 import QtGui, QtWidgets
 from random import randint as randm
+from PyQt5 import QtGui, QtWidgets
 import Assets
 
 #The following class sets up the application window and executes the fuctionality of the
 #application
-class UI_setup():
+class UISetup():
     def __init__(self,window):
         super().__init__()
-        
         #Private variables for character/complexity options
         self._characters = []
         self._alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         self._nums =  "0123456789"
-        self._chars = "'"+'!"#$%&()*+,-./:;<=>?@[]^_`{|}~' 
+        self._chars = "'"+'!"#$%&()*+,-./:;<=>?@[]^_`{|}~'
         
         #window settings
         window.setWindowTitle("PasswordGenerator")
         window.resize(600,145)
         window.setMaximumSize(700,150)
-        window.setWindowIcon(QtGui.QIcon(':/Icons/PasswordIcon.png'))
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(":/Icons/PasswordIcon.png"))
+        window.setWindowIcon(icon)
         
         #window layout settings
         self.centralwidget = QtWidgets.QWidget(window)
-        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
-        self.horizontalLayout = QtWidgets.QHBoxLayout()
-        self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
+        self.vertical_layout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.horizontal_layout = QtWidgets.QHBoxLayout()
+        self.horizontal_layout_2 = QtWidgets.QHBoxLayout()
         self.spacer = QtWidgets.QSpacerItem(40, 20)
         
         #Font settings
@@ -48,69 +48,73 @@ class UI_setup():
         #Prompt label settings
         self.prompt = QtWidgets.QLabel("Specify the desired password length :")
         self.prompt.setFont(self.font)
-        self.horizontalLayout.addWidget(self.prompt)
+        self.horizontal_layout.addWidget(self.prompt)
         
         #Input or entry widget setup
-        self.lineEdit = QtWidgets.QLineEdit("")
-        self.lineEdit.setFont(self.font)
-        self.lineEdit.setFrame(False)
-        self.lineEdit.setStyleSheet(u"background-color:rgb(240,240,230);")
-        self.horizontalLayout.addWidget(self.lineEdit)
+        self.line_edit = QtWidgets.QLineEdit("")
+        self.line_edit.setFont(self.font)
+        self.line_edit.setFrame(False)
+        self.line_edit.setStyleSheet("background-color:rgb(240,240,230);")
+        self.horizontal_layout.addWidget(self.line_edit)
         
-        self.verticalLayout.addLayout(self.horizontalLayout)
+        self.vertical_layout.addLayout(self.horizontal_layout)
         
         #Complexity or character options selection radio buttons
-        self.ABC = QtWidgets.QRadioButton("ABCD", toggled = lambda:self.selected(self._alphabets,self.ABC))
+        self.ABC = QtWidgets.QRadioButton("ABCD", toggled = lambda:
+                                        self.selected(self._alphabets,self.ABC))
         self.ABC.setFont(self.font)
         self.ABC.setAutoExclusive(False)
-        self.horizontalLayout_2.addWidget(self.ABC)
-        self.horizontalLayout_2.addItem(self.spacer)
+        self.horizontal_layout_2.addWidget(self.ABC)
+        self.horizontal_layout_2.addItem(self.spacer)
         
-        self.abc = QtWidgets.QRadioButton("abcd", toggled = lambda:self.selected(self._alphabets.lower(),self.abc))
+        self.abc = QtWidgets.QRadioButton("abcd", toggled = lambda:
+                                        self.selected(self._alphabets.lower(),self.abc))
         self.abc.setFont(self.font)
         self.abc.setAutoExclusive(False)
-        self.horizontalLayout_2.addWidget(self.abc)
-        self.horizontalLayout_2.addItem(self.spacer)
+        self.horizontal_layout_2.addWidget(self.abc)
+        self.horizontal_layout_2.addItem(self.spacer)
 
-        self.numbers = QtWidgets.QRadioButton("1234", toggled = lambda:self.selected(self._nums, self.numbers))
+        self.numbers = QtWidgets.QRadioButton("1234", toggled = lambda:
+                                            self.selected(self._nums, self.numbers))
         self.numbers.setFont(self.font)
         self.numbers.setAutoExclusive(False)
-        self.horizontalLayout_2.addWidget(self.numbers)
-        self.horizontalLayout_2.addItem(self.spacer)
+        self.horizontal_layout_2.addWidget(self.numbers)
+        self.horizontal_layout_2.addItem(self.spacer)
 
-        self.special_chars = QtWidgets.QRadioButton("@#$*", toggled = lambda:self.selected(self._chars,self.special_chars))
+        self.special_chars = QtWidgets.QRadioButton("@#$*", toggled = lambda:
+                                                self.selected(self._chars,self.special_chars))
         self.special_chars.setFont(self.font)
         self.special_chars.setAutoExclusive(False)
-        self.horizontalLayout_2.addWidget(self.special_chars)
-        self.horizontalLayout_2.addItem(self.spacer)
+        self.horizontal_layout_2.addWidget(self.special_chars)
+        self.horizontal_layout_2.addItem(self.spacer)
         
         #Enter button settings
-        self.EnterButton = QtWidgets.QPushButton("Enter", clicked = lambda:self.generatePassword())
-        self.EnterButton.setFixedSize(75,27)
-        self.EnterButton.setFont(self.font)
-        self.horizontalLayout_2.addWidget(self.EnterButton)
+        self.enter_button = QtWidgets.QPushButton("Enter", clicked = lambda:
+                                                self.generate_password())
+        self.enter_button.setFixedSize(75,27)
+        self.enter_button.setFont(self.font)
+        self.horizontal_layout_2.addWidget(self.enter_button)
         
-        self.verticalLayout.addLayout(self.horizontalLayout_2)
+        self.vertical_layout.addLayout(self.horizontal_layout_2)
         
         #Output label settings
         self.output = QtWidgets.QLineEdit("")
         self.output.setFont(self.font)
         self.output.setFrame(False)
         self.output.setReadOnly(True)
-        self.output.setStyleSheet(u"background-color:rgb(240,240,230);")
-        self.verticalLayout.addWidget(self.output)
+        self.output.setStyleSheet("background-color:rgb(240,240,230);")
+        self.vertical_layout.addWidget(self.output)
         
         window.setCentralWidget(self.centralwidget)       
         
     #This function generates a password containing the selected characters and the 
     #given length, and then displays it on the screen. The returned password can be copied
-    def generatePassword(self): 
-            
-        input = self.lineEdit.text()
+    def generate_password(self):     
+        length = self.line_edit.text()
         password = ""
                        
         try:
-            length = int(input)
+            length = int(length)
         except ValueError:
             self.output.setText('Specified length must be a whole number')
         else:
@@ -119,7 +123,8 @@ class UI_setup():
                     
             #If none of the options are selected, the application prompts the user to
             #select one before the application continues to run
-            elif self.ABC.isChecked()==self.abc.isChecked()==self.numbers.isChecked()==self.special_chars.isChecked()==False:
+            elif self.ABC.isChecked()==self.abc.isChecked()==self.numbers.isChecked()==\
+                    self.special_chars.isChecked()==False:
                 self.output.setText("You have to select at least one option")
                 
             #Prints a random password of the specified length
@@ -131,9 +136,8 @@ class UI_setup():
                 self.output.setText(f'Password is : {password}')
                 
     #Slot(function) gets executed when any of the radio buttons is selected or 
-    # unselected (toggled)
+    #unselected (toggled)
     def selected(self,chars, option):
-            
         if option.isChecked() and chars not in self._characters:
             self._characters.append(chars)
         else:
@@ -143,7 +147,8 @@ class UI_setup():
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    PasswordGenerator = QtWidgets.QMainWindow()
-    ui = UI_setup(PasswordGenerator)
-    PasswordGenerator.show()
+    passwordGenerator = QtWidgets.QMainWindow()
+    ui = UISetup(passwordGenerator)
+    passwordGenerator.show()
     sys.exit(app.exec_())
+    
